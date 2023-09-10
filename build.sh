a0d182ee6325ba895698832dee81da2d5053467c
Michael Bitokhov <michael.bitokhov@gmail.com>
2023-09-10 03:24:57 -0400

    Remove unnecessary build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-inotifywait -r -m -e close_write styles |
-  while read file_path file_event file_name; do
-    echo $file_event
-    yarn lessc styles/styles.less -x >public/styles.css
-  done
